fd483e8817ce93990edc3ca02f449176865afb7b
Shawn <botider@gmail.com>
2019-08-25 05:34:57 +0800

    開場攝影機移動動畫

---
+++ Assets/MainCamera/CamStartAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamStartAnim : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        GetComponent<Animator>().SetTrigger("PlayStartAnim");
+        Debug.Log("AnimStart");
+    }
+
+    private void TurnOffAnimator()
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+
+}
--- Assets/Script/Sunny/CameraFollow.cs
+++ Assets/Script/Sunny/CameraFollow.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
+    private Quaternion iniRotation;
+    private bool isInit;
+    void Awake()
+    {
+        isInit = false;
+        iniRotation = transform.rotation;
+    }
     void Update()
     {
+        if(!isInit)
+        {
+            iniRotation = transform.rotation;
+            isInit = true;
+            Debug.Log("CamInit");
+        }
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3, player.transform.position.z-3);
     }
 }
