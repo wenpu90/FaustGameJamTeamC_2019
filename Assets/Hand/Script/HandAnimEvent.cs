4d0c0f0e390c3c805ae21b90191f7c189f29eb37
Shawn <botider@gmail.com>
2019-08-24 17:30:10 +0800

    手攻擊

--- Assets/Hand/Script/HandAnimEvent.cs
+++ Assets/Hand/Script/HandAnimEvent.cs
@@ -4,30 +4,51 @@ using UnityEngine;
 
 public class HandAnimEvent : MonoBehaviour
 {
+    public bool IsAttacking;
     public ParticleSystem[] particles;
     [Range(0.001f, 0.1f)]public float shakeAmount;
     [Range(0.1f, 5f)] public float shakeDuration;
+    public Transform target;
+    public Transform parent;
+    private bool IsFaceTO;
+    public Vector3 HandMoveOffset;
+    [Range(0.01f, 1f)] public float HandMoveSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (target == null) target = GameObject.FindGameObjectWithTag("Player").transform;
+        if (parent == null) parent = this.transform.parent;
+    }
+
+    public void SetAttackingTrue()
+    {
+        IsAttacking = true;
+    }
+    public void SetAttackingFalse()
+    {
+        IsAttacking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Anim 事件開關
+        if(IsFaceTO)
+        {
+            FaceTo();
+        }
+
     }
 
-    public void HandSlamEvent()
+    private void HandSlamEvent()
     {
         particles[0].Play(true);
         StartCoroutine(CameraShake());
         
     }
 
-    public IEnumerator CameraShake()
+    private IEnumerator CameraShake()
     {
         var cam = Camera.main;
         if(cam != null)
@@ -43,7 +64,28 @@ public class HandAnimEvent : MonoBehaviour
                 yield return null;
             }
             cam.transform.localPosition = oPos;
-
         }
     }
+
+    private void FaceTo()
+    {
+        if (target == null) return;
+
+        var pos = target.position;
+        pos = new Vector3(pos.x, this.transform.position.y, pos.z);
+        var dir = pos - this.transform.position;
+
+        this.transform.LookAt(pos);
+    }
+
+    public void SetFaceToTrue()
+    {
+        IsFaceTO = true;
+    }
+    public void SetFaceToFalse()
+    {
+        IsFaceTO = false;
+    }
+
+
 }
--- Assets/Hand/Script/ThisIsHand.cs
+++ Assets/Hand/Script/ThisIsHand.cs
@@ -1,14 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ThisIsHand : MonoBehaviour
 {
-
+    public float moveThreshold = 0.01f;
+    public HandAnimEvent animEvent;
     public Animator anim;
     public int RandomRange;
     public KeyCode a,b,c,d,e;
     public Transform target;
+    public bool IsTestAnim;
+    public Vector3 HandMoveOffset;
+    public HandState state;
+    public bool IsAttack;
+    public bool IsUp;
+    public Vector3 originalPos;
+    [SerializeField] Vector3 _targetPosition;
+    public enum HandState
+    {
+        Idle,
+        moveUp,
+        attack,
+        moveDown
+    }
+
+    [Range(0.01f,1f)]public float HandMoveSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,22 +35,88 @@ public class ThisIsHand : MonoBehaviour
 
     private void OnEnable()
     {
-        PlayAnimRandom();
+        state = HandState.moveUp;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(IsTestAnim)
         {
-            FaceTo();
-            Slam();
+            SlamAttack();
         }
+        if(Input.GetMouseButtonDown(0) && IsUp==false)
+        {
+            StartCoroutine(MoveUp(()=>SlamAttack()));
+            
+        }
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Default"))
+        {
+            if(IsUp && !animEvent.IsAttacking)
+            {
+                Debug.LogError("DownAnim");
+                
+                StartCoroutine(MoveDown());
+            }
+        }
+
+
     }
 
-    public void Slam()
+
+
+
+    public IEnumerator MoveUp(Action attack)
     {
+        Debug.Log("HandMoveUp");
+        originalPos = transform.position;
+        _targetPosition = target.position;
+        var pos = HandMoveOffset + target.position;
+        var targetPos = new Vector3(transform.position.x, pos.y, transform.position.z);
+        //Debug.LogError(target.position);
+        while (transform.position.y <= targetPos.y-0.01f)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPos, HandMoveSpeed);
+            Debug.Log("HandUPing");
+            yield return null;
+        }
+        transform.position = targetPos;
+        IsUp = true;
+        attack.Invoke();
+        animEvent.IsAttacking = true;
+
+
+    }
+
+
+
+    public IEnumerator MoveDown(System.Action next = null)
+    {
+        Debug.Log("HandDown");
+        var pos = originalPos-HandMoveOffset;
+        var targetPos = new Vector3(transform.position.x, pos.y, transform.position.z);
+        while (transform.position.y > targetPos.y+0.01f)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPos, HandMoveSpeed);
+            yield return null;
+            Debug.Log("HandDowning");
+        }
+        transform.position = targetPos;
+        IsUp = false;
+    }
+
+    //bool AnimatorIsPlaying()
+    //{
+    //    return anim.GetCurrentAnimatorStateInfo(0).length >
+    //           anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    //}
+
+    public void SlamAttack()
+    {
+        
         anim.SetTrigger("Slam");
+        
     }
 
     public void FaceTo()
@@ -48,7 +132,7 @@ public class ThisIsHand : MonoBehaviour
 
     public void PlayAnimRandom()
     {
-        int num = Random.Range(0, RandomRange);
+        int num = UnityEngine.Random.Range(0, RandomRange);
 
         anim.SetTrigger(num);
     }
